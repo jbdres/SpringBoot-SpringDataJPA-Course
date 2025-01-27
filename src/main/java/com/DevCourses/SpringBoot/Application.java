ae9222543d933dddaa09394c408e079681302593
jbdres <jbdres_@outlook.com>
2025-01-27 15:19:46 -0500

    Value annotation and custom .properties files

--- src/main/java/com/DevCourses/SpringBoot/Application.java
+++ src/main/java/com/DevCourses/SpringBoot/Application.java
@@ -11,9 +11,19 @@ public class Application {
         var ctx = SpringApplication.run(Application.class, args);
 
         MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-        System.out.println(myFirstService.getJavaVersion());
-        System.out.println(myFirstService.getOSName());
-        System.out.println(myFirstService.getApplicationProperty());
+        System.out.println(myFirstService.getCountry());
+        System.out.println(myFirstService.getYear());
+        System.out.println(myFirstService.getProfile());
+        System.out.println(myFirstService.getAge());
+        System.out.println(myFirstService.getLaptopManufacter());
+        System.out.println(myFirstService.getLaptopModel());
+        System.out.println(myFirstService.getLaptopProcessor());
+        System.out.println(myFirstService.getLaptopInstalledRAM());
+        System.out.println(myFirstService.getLaptopSystemType());
+        System.out.println(myFirstService.getPhoneManufacter());
+        System.out.println(myFirstService.getPhoneModel());
+        System.out.println(myFirstService.getPhoneCameraResolution());
+        System.out.println(myFirstService.getPhoneSoVersion());
 
     }
 
